2035bc043d1f3034b8e05d6b4d8d42cda797337e
emma061219 <emmag061219@outlook.com>
2018-05-21 17:20:19 +1200

    Commit two

--- collision_detection/script.js
+++ collision_detection/script.js
@@ -9,3 +9,56 @@ Next, add the following features:
 - create an item that makes the puck smaller
 
 */
+
+//declaring variables for puck
+var puck_x = 210;
+var puck_y = 210;
+var speed = 50
+var puck = document.getElementById('puck');
+var gap = document.getElementById('gap');
+
+function logCoordinates(){
+    console.log('x=' + puck_x + 'y=' + puck_y)
+}
+
+function checkCollision(){
+    if (puck_x >= 250 && puck_x <= 350){
+        gap.style.backgroundColor = 'red';
+    }
+    else{
+        gap.style.backgroundColor = "white";
+    }
+}
+
+document.getElementById('puck').style.top = puck_y + 'px';
+document.getElementById('puck').style.left = puck_x + 'px';
+
+//move left
+document.getElementById('left').addEventListener('click', function(){
+    puck_x -= speed;
+    document.getElementById('puck').style.left = puck_x + 'px';
+    console.log('x=' + puck_x + 'y=' + puck_y)
+    checkCollision();
+});
+
+//move right
+document.getElementById('right').addEventListener('click', function(){
+    puck_x += speed;
+    document.getElementById('puck').style.left = puck_x + 'px';
+    console.log('x=' + puck_x + 'y=' + puck_y)
+    checkCollision();
+});
+//move up
+document.getElementById('up').addEventListener('click', function(){
+    puck_y -= speed;
+    document.getElementById('puck').style.top = puck_y + 'px';
+    console.log('x=' + puck_x + 'y=' + puck_y)
+    checkCollision();
+});
+//move down
+document.getElementById('down').addEventListener('click', function(){
+    puck_y += speed;
+    document.getElementById('puck').style.top = puck_y + 'px';
+    console.log('x=' + puck_x + 'y=' + puck_y)
+    checkCollision();
+});
